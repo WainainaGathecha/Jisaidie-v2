d0bca8b13f2796367f12ee5c8667e7f9a22e94d1
Wainaina Gathecha <wainainagathecha@gmail.com>
2026-04-13 13:51:13 +0300

    add header

---
+++ script.js
@@ -0,0 +1,8 @@
+// shared interaction code
+// Hamburger menu
+const hamburger = document.getElementById('hamburger');
+const mobileMenu = document.getElementById('mobile-menu');
+
+hamburger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+})
